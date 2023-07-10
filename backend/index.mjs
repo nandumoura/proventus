d00441bba494be6eda3f5edc4a85f2e8b807e7e6
Fernando Moura <nandumoura@gmail.com>
2023-07-10 14:14:46 -0300

    feat: add cors

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,10 +1,11 @@
 // index.mjs
 import Express from "express";
 import projects from "./routers/projectRouter.mjs"
-
+import cors from "cors"
 const app = Express();
 
 app.use(Express.json());
+app.use(cors())
 app.use("/projects",projects)
 
 const port = process.env.PORT;
