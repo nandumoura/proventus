3ca637ae28c6db81093aebba81d0433b7c6a9f17
Fernando Moura <nandumoura@gmail.com>
2023-07-14 16:36:16 -0300

    refactor: remove unecessary sections

--- frontend/frontend-proventus/src/components/Sidebar.tsx
+++ frontend/frontend-proventus/src/components/Sidebar.tsx
@@ -1,10 +1,5 @@
-import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 const Sidebar = () => {
-  const profileRef = useRef<HTMLButtonElement | null>(null);
-
-  const [isProfileActive, setIsProfileActive] = useState(false);
-
   const navigation = [
     {
       href: "/",
@@ -88,82 +83,6 @@ const Sidebar = () => {
     },
   ];
 
-  const navsFooter = [
-    {
-      href: "/",
-      name: "Help",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-          />
-        </svg>
-      ),
-    },
-    {
-      href: "/",
-      name: "Settings",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-          />
-        </svg>
-      ),
-    },
-    {
-      href: "/",
-      name: "Logout",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-          />
-        </svg>
-      ),
-    },
-  ];
-
-  useEffect(() => {
-    const handleProfile = (e: MouseEvent) => {
-      if (profileRef.current && !profileRef.current.contains(e.target as Node))
-        setIsProfileActive(false);
-    };
-    document.addEventListener("click", handleProfile);
-  }, []);
-
   return (
     <>
       <nav className="fixed top-0 left-0 w-20 h-full border-r bg-white space-y-8">
@@ -195,79 +114,6 @@ const Sidebar = () => {
                 </li>
               ))}
             </ul>
-            <div>
-              <ul className="px-4 pb-4 text-sm font-medium">
-                {navsFooter.map((item, idx) => (
-                  <li key={idx}>
-                    <Link
-                      to={item.href}
-                      className="relative flex items-center justify-center gap-x-2 text-gray-600 p-2 rounded-lg  hover:bg-gray-50 active:bg-gray-100 duration-150 group"
-                    >
-                      <div className="text-gray-500">{item.icon}</div>
-                      <span className="absolute left-14 p-1 px-1.5 rounded-md whitespace-nowrap text-xs text-white bg-gray-800 hidden group-hover:inline-block group-focus:hidden duration-150">
-                        {item.name}
-                      </span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <div className="relative py-4 px-4 border-t">
-                <button
-                  ref={profileRef}
-                  className="w-12 h-12 flex items-center gap-x-4 cursor-pointer rounded-full ring-offset-2 ring-gray-800 focus:ring-2 duration-150"
-                  onClick={() => setIsProfileActive(!isProfileActive)}
-                >
-                  <img
-                    width={150}
-                    height={150}
-                    alt=""
-                    src="https://randomuser.me/api/portraits/women/79.jpg"
-                    className="w-12 h-12 rounded-full"
-                  />
-                </button>
-                {isProfileActive ? (
-                  <div className="absolute bottom-4 left-20 w-64 rounded-lg bg-white shadow-md border text-sm text-gray-600">
-                    <div className="p-2">
-                      <span className="block text-gray-500/80 p-2">
-                        vienna@gmail.com
-                      </span>
-                      <Link
-                        to="/implementar"
-                        className="block w-full p-2 text-left rounded-md hover:bg-gray-50 active:bg-gray-100 duration-150"
-                      >
-                        Status
-                      </Link>
-                      <div className="relative rounded-md hover:bg-gray-50 active:bg-gray-100 duration-150">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="w-4 h-4 absolute right-1 inset-y-0 my-auto pointer-events-none"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                        <select className="w-full cursor-pointer appearance-none bg-transparent p-2 outline-none">
-                          <option disabled selected>
-                            Theme
-                          </option>
-                          <option>Dark</option>
-                          <option>Light</option>
-                        </select>
-                      </div>
-                      <button className="block w-full p-2 text-left rounded-md hover:bg-gray-50 active:bg-gray-100 duration-150">
-                        Logout
-                      </button>
-                    </div>
-                  </div>
-                ) : (
-                  ""
-                )}
-              </div>
-            </div>
           </div>
         </div>
       </nav>
