2c4d0fc976ce02f692979c1916d645222333bf9b
Fernando Moura <nandumoura@gmail.com>
2023-07-14 16:02:56 -0300

    fix: change the way to receive data

--- backend/routers/errorLogsRouter.mjs
+++ backend/routers/errorLogsRouter.mjs
@@ -5,7 +5,8 @@ import { createErrorLog } from "../services/errorLogsServices.mjs";
 // send an object with name as namefrom error is and error message
 router.post("/", async (req, res) => {
   try {
-    await createErrorLog(req.body.name, req.body.error);
+    const { name, error } = req.body.data;
+    await createErrorLog(name, error);
     res.send({ success: true });
   } catch (error) {
     console.error(error);
